d1b36d433b57d139ad1c4ca4cac65d6424a8497d
Sebastian Kaspari <s.kaspari@gmail.com>
2012-12-17 08:08:13 +0100

    Add simple about view.

--- src/com/androidzeitgeist/procrastination/fragment/AboutFragment.java
+++ src/com/androidzeitgeist/procrastination/fragment/AboutFragment.java
@@ -5,13 +5,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView;
+import android.widget.TextView;
 
 import com.androidzeitgeist.procrastination.R;
+import com.androidzeitgeist.procrastination.helper.ApplicationHelper;
 
 public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+
+        TextView versionView = (TextView) view.findViewById(R.id.version);
+        versionView.setText(ApplicationHelper.getVersion(getActivity()));
+
+        return view;
     }
 }
